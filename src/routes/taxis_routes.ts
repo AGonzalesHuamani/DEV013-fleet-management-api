163c0f6bdceaf64a5978615a39a06321603abab1
Antone Gonzales <es.angelika@gmail.com>
2024-05-02 16:47:01 -0500

    se agrego la estructura para get taxis para obtener listado de taxis en taxis_routes.ts

--- src/routes/taxis_routes.ts
+++ src/routes/taxis_routes.ts
@@ -2,4 +2,37 @@ import express, { Router } from 'express';
 
 const router: Router = express.Router();
 
-// Agregar otros enrutadores aquí si es necesario
+/**
+ * Get track
+ * @swagger
+ * /taxis:
+ *    get:
+ *      tags:
+ *        - Taxis
+ *      summary: "Obtener taxis"
+ *      description: Este endpoint es para obtener listado de taxis
+ *      parameters:
+ *        - in: query
+ *          name: skip
+ *          schema:
+ *            type: integer
+ *            minimum: 1
+ *          description: Número de página que se desea obtener
+ *        - in: query
+ *          name: take
+ *          schema:
+ *            type: integer
+ *            minimum: 1
+ *            maximum: 100
+ *            default: 10
+ *          description: Número máximo de resultados por página
+ *      responses:
+ *        '200':
+ *          description: Operación exitosa.
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: array
+ *                      items:
+ *                          $ref: '#/components/schemas/Taxis'
+ */
