c487827b915f20f6ae97422d8178a75f2b0e2dab
Antone Gonzales <es.angelika@gmail.com>
2024-05-02 14:14:14 -0500

    se agrego la estructura del swagger para la documentacion de la API

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import express, { Application, Request, Response } from 'express';
+
+//swagger
 import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import { options } from './swaggerOption'
+
 
 const app: Application = express();
-
 const PORT: number = 3001;
 
+// middleware para parsear el body de las request
 app.use(express.json());
 
+const specs = swaggerJSDoc(options)
+app.use("/documentation", swaggerUi.serve, swaggerUi.setup(specs))
 
-app.use("/documentation", swaggerUi.serve)
 app.use('/', (req: Request, res: Response): void => {
     res.send('Hello world!');
 });
@@ -16,3 +22,5 @@ app.use('/', (req: Request, res: Response): void => {
 app.listen(PORT, (): void => {
     console.log('SERVER IS UP ON PORT:', PORT);
 });
+
+export default app;
